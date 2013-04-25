fb39980356bc3678aa0966028847ffe604711718
abigail sosa <abigail.sosa.h@gmail.com>
2013-04-25 09:33:27 -0500

    Change offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ActiveRecord::Base
-  attr_accessible :date, :message, :status
+  attr_accessible :message, :status, :reply_message
 
-  has_one :receiver, :class_name => 'User', :foreign_key => 'sender_id'
+  has_one :receiver, :class_name => 'User', :foreign_key => 'receiver_id'
   belongs_to :sender, :class_name => 'User'
   has_one :disk
 end

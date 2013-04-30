5c3916c0731538699d92bbf2db34b08bf0b0e585
abigail sosa <abigail.sosa.h@gmail.com>
2013-04-29 21:59:16 -0500

    Offer model removed

--- app/models/offer.rb
+++
@@ -1,7 +0,0 @@
-class Offer < ActiveRecord::Base
-  attr_accessible :message, :status, :reply_message
-
-  has_one :receiver, :class_name => 'User', :foreign_key => 'receiver_id'
-  belongs_to :sender, :class_name => 'User'
-  has_one :disk
-end

fcd3ca1226674ec5e69059226a75bd522d16c38e
Samuel Heaney <sheaney@gmail.com>
2013-05-02 19:01:25 -0500

    Remove password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ActiveRecord::Base
 
   validates :username, :presence => true,
             :uniqueness => true
-  validates :mail, :presence => true
-  validates :password, :presence => true
-  validates_uniqueness_of :username
+  #validates :mail, :presence => true
   
 end
